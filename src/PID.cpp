f1d6ce761ee9a40a184e5a6744b87142840a32e5
Ivan Danov <(none)>
2017-05-20 16:16:42 +0100

    Implement the PID calculations

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,7 +1,5 @@
 #include "PID.h"
 
-using namespace std;
-
 /*
 * TODO: Complete the PID class.
 */
@@ -11,11 +9,17 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    this->d_error = cte - this->p_error;
+    this->i_error += cte;
+    this->p_error = cte;
 }
 
 double PID::TotalError() {
+  return -this->Kp * this->p_error - this->Kd * this->d_error - this->Ki * this->i_error;
 }
-
